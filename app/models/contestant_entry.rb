c0781a28a09a546d360ab2aefce8824e0b29ff36
Chad Westfall <chadmwestfall@yahoo.com>
2011-03-08 22:28:49 -0500

    fixed db adapter, added fancyzoom

--- app/models/contestant_entry.rb
+++ app/models/contestant_entry.rb
@@ -5,7 +5,7 @@ class ContestantEntry < ActiveRecord::Base
   
   has_many :votes
   
-  has_attached_file :photo, :styles => { :medium => "240x360>", :thumb => "48x72>" }
+  has_attached_file :photo, :styles => { :xl => "480x720>", :large => "320x480>", :medium => "240x360>", :thumb => "48x72>" }
   validates_attachment_presence :photo
   validates_attachment_content_type :photo, :content_type => 'image/jpeg'
   
